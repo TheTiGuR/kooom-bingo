ad9403c4925bd73e2ccb5deda76f3eb50815c0dc
Chris SImiskey <thetigur@gmail.com>
2022-02-09 21:53:34 -0700

    Initial commit

---
+++ models/song.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+let SongSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    artist: {
+      type: String,
+    },
+  },
+  {
+    collection: 'songs',
+  }
+);
+
+export default mongoose.model('Song', SongSchema);
